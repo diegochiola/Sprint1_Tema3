e6e02cd33476e21f2edc16cf000dd2ab86645cd0
diega <diegochiola@gmail.com>
2023-12-18 10:33:17 +0100

    agregue la funcion count

--- Ejercicio2_php.php
+++ Ejercicio2_php.php
@@ -4,13 +4,17 @@ $x = array(10, 20, 30, 40, 50, 60);
 
 //imprimir por pantalla $x
 var_dump($x);//incluye el tipo de valor
-//print_r($x); solo muestra su contenido en cada psoicion
 
+//mostrar tamaño del array con count
+echo count($x);
+
+//print_r($x); solo muestra su contenido en cada psoicion
 //echo $x[0]; imprimir una sola posicion
 
 //eliminar un elemento
 unset ($x[1]);
 print_r($x);
+echo count($x);
 
 //claves enteras deben ser normalizadas 
 
