cd6b0c98f5566c956d92a827f93e78b4ac1d94d9
diega <diegochiola@gmail.com>
2023-12-19 09:49:01 +0100

    Ejercicio4

---
+++ Ejercicio4_php.php
@@ -0,0 +1,8 @@
+<?php
+//Creo un array
+$datosPersonales = array('nombre' => "Diega", 'edad' => 31, 
+                        'email'=> "diegachiola@gmail.com",'comida favorita' => "pizza" );
+
+var_dump($datosPersonales);  
+
+?>
